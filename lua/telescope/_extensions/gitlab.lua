626545e0e0e588c2a2bcca9128245df54118612f
Luciano Furtado <lrfurtado@gmail.com>
2023-11-21 16:17:09 -0800

    Added support for searching blobs and finding projects

--- lua/telescope/_extensions/gitlab.lua
+++ lua/telescope/_extensions/gitlab.lua
@@ -3,11 +3,6 @@ if not has_telescope then
   error "This plugins requires nvim-telescope/telescope.nvim"
 end
 
-local has_glab = vim.fn.executable "glab" == 1
-if not has_glab then
-  error "This plugin requires the glab binary"
-end
-
 local pickers = require "telescope.pickers"
 local finders = require "telescope.finders"
 local previewers = require "telescope.previewers"
@@ -46,7 +41,7 @@ local glab_command = function(args, opts)
   if opts.fields then
     for k, v in pairs(opts.fields) do
       table.insert(_args, "-f")
-      table.insert(_args, string.format("%s=%s", k, v))
+      table.insert(_args, string.format("'%s=%s'", k, v))
     end
   end
 
@@ -135,8 +130,8 @@ local glab_issues = function(opts)
       end,
       previewer = previewers.new_buffer_previewer {
         title = "gitlab issue preview",
-        define_preview = function(self, entry, status)
-          content_table = {}
+        define_preview = function(self, entry, _)
+          local content_table = {}
           table.insert(content_table, string.format("Title     : %s", entry.value.title))
           table.insert(content_table, string.format("URL       : %s", entry.value.web_url))
           table.insert(content_table, string.format("Severity  : %s", entry.value.severity))
@@ -234,8 +229,8 @@ local glab_mrs = function(opts)
       end,
       previewer = previewers.new_buffer_previewer {
         title = "gitlab issue preview",
-        define_preview = function(self, entry, status)
-          content_table = {}
+        define_preview = function(self, entry, _)
+          local content_table = {}
           table.insert(content_table, string.format("Title     : %s", entry.value.title))
           table.insert(content_table, string.format("URL       : %s", entry.value.web_url))
 
@@ -246,10 +241,147 @@ local glab_mrs = function(opts)
     :find()
 end
 
+local glab_repos = function(opts)
+  opts = opts or {}
+
+  if not opts.fields then
+    opts.fields = {}
+  end
+
+  if opts.search then
+    opts.fields.search = opts.search
+  end
+
+  opts.fields.include_subgroups = true
+  opts.fields.simple = true
+
+  local groupId = vim.g.glab_group_id
+  if not groupId then
+    utils.notify("telescope-gitlab", {
+      msg = "This plugin requires the glab_group_id global variable to be set",
+      level = "ERROR",
+    })
+    return
+  end
+  local output = get_glab_command_json({ string.format("/groups/%d/projects", groupId) }, opts)
+
+  local o = vim.json.decode(table.concat(output, ""))
+  local repos = {}
+  for _, v in ipairs(o) do
+    table.insert(repos, v.value)
+  end
+
+  pickers
+    .new(opts, {
+      prompt_title = "Glab Repos",
+      finder = finders.new_table {
+        results = repos,
+        entry_maker = function(entry)
+          local content = string.format("%s", entry.web_url)
+          return {
+            value = entry,
+            display = content,
+            ordinal = content,
+          }
+        end,
+      },
+      sorter = conf.generic_sorter(opts),
+      attach_mappings = function(prompt_bufnr, map)
+        actions.select_default:replace(function()
+          actions.close(prompt_bufnr)
+          local selection = action_state.get_selected_entry()
+          utils.get_os_command_output { "open", selection.value.web_url }
+        end)
+        local run_command = function()
+          local selection = action_state.get_selected_entry()
+        end
+
+        map("n", "c", run_command)
+        return true
+      end,
+      previewer = previewers.new_buffer_previewer {
+        title = "gitlab repo preview",
+        define_preview = function(self, entry, _)
+          local content_table = {}
+          table.insert(content_table, string.format("URL       : %s", entry.value.web_url))
+
+          vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, content_table)
+        end,
+      },
+    })
+    :find()
+end
+
+local glab_search = function(opts)
+  opts = opts or {}
+
+  opts.scope = opts.scope or "blobs"
+
+  if not opts.fields then
+    opts.fields = {}
+  end
+
+  if opts.scope then
+    opts.fields.scope = opts.scope
+  end
+
+  if opts.query then
+    opts.fields.search = opts.query
+  end
+
+  local groupId = vim.g.glab_group_id
+  local output = get_glab_command_json({ string.format("/groups/%d/search", groupId) }, opts)
+
+  local o = vim.json.decode(table.concat(output, ""))
+  log.info("glab_search: ", vim.inspect(o))
+  local results = {}
+  local enc = require "url_encoder"
+  for _, v in ipairs(o) do
+    if opts.scope == "blobs" then
+      v.value.title = string.format("%s (%s)", v.value.path, v.value.project_id)
+      v.value.web_url = string.format(
+        "https://gitlab.com/api/v4/projects/%s/repository/files/%s/raw?ref=%s",
+        v.value.project_id,
+        enc.encode(v.value.path),
+        v.value.ref
+      )
+    end
+    table.insert(results, v.value)
+  end
+
+  pickers
+    .new(opts, {
+      prompt_title = "Glab Search",
+      finder = finders.new_table {
+        results = results,
+        entry_maker = function(entry)
+          local content = string.format("%s", entry.title)
+          return {
+            value = entry,
+            display = content,
+            ordinal = content,
+          }
+        end,
+      },
+      sorter = conf.generic_sorter(opts),
+      attach_mappings = function(prompt_bufnr, _)
+        actions.select_default:replace(function()
+          actions.close(prompt_bufnr)
+          local selection = action_state.get_selected_entry()
+          utils.get_os_command_output { "open", selection.value.web_url }
+        end)
+        return true
+      end,
+    })
+    :find()
+end
+
 return telescope.register_extension {
-  setup = function(optsExt, opts) end,
+  setup = function() end,
   exports = {
     issues = glab_issues,
     mrs = glab_mrs,
+    repos = glab_repos,
+    search = glab_search,
   },
 }
