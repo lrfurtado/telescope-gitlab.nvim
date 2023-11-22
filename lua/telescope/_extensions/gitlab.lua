4acb4a2f93016419d04fba179c29b676b4e5bb17
Luciano Furtado <lrfurtado@gmail.com>
2023-11-21 20:40:37 -0800

    Added support for searching blobs and finding projects

--- lua/telescope/_extensions/gitlab.lua
+++ lua/telescope/_extensions/gitlab.lua
@@ -28,6 +28,35 @@ if not has_jq then
   })
 end
 
+
+local char_to_hex = function(c)
+  return string.format("%%%02X", string.byte(c))
+end
+
+local function urlencode(url)
+  if url == nil then
+    return
+  end
+  url = url:gsub("\n", "\r\n")
+  url = url:gsub("([^%w ])", char_to_hex)
+  url = url:gsub(" ", "+")
+  return url
+end
+
+local hex_to_char = function(x)
+  return string.char(tonumber(x, 16))
+end
+
+local urldecode = function(url)
+  if url == nil then
+    return
+  end
+  url = url:gsub("+", " ")
+  url = url:gsub("%%(%x%x)", hex_to_char)
+  return url
+end
+
+
 local glab_command = function(args, opts)
   opts = opts or {}
 
@@ -335,14 +364,13 @@ local glab_search = function(opts)
   local o = vim.json.decode(table.concat(output, ""))
   log.info("glab_search: ", vim.inspect(o))
   local results = {}
-  local enc = require "url_encoder"
   for _, v in ipairs(o) do
     if opts.scope == "blobs" then
       v.value.title = string.format("%s (%s)", v.value.path, v.value.project_id)
       v.value.web_url = string.format(
         "https://gitlab.com/api/v4/projects/%s/repository/files/%s/raw?ref=%s",
         v.value.project_id,
-        enc.encode(v.value.path),
+        urlencode(v.value.path),
         v.value.ref
       )
     end
